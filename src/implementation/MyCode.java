0066ec547b5cd7c66db7288630d5dfa55739c276
mdragonborn <lena.markovic996@gmail.com>
2019-05-31 18:00:48 +0200

    Init

---
+++ src/implementation/MyCode.java
@@ -0,0 +1,117 @@
+package implementation;
+
+import java.util.Enumeration;
+
+import code.GuiException;
+import x509.v3.CodeV3;
+
+public class MyCode extends CodeV3 {
+
+	public MyCode(boolean[] algorithm_conf, boolean[] extensions_conf, boolean extensions_rules) throws GuiException {
+		super(algorithm_conf, extensions_conf, extensions_rules);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean canSign(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean exportCSR(String arg0, String arg1, String arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean exportCertificate(String arg0, String arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean exportKeypair(String arg0, String arg1, String arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getCertPublicKeyAlgorithm(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCertPublicKeyParameter(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSubjectInfo(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean importCAReply(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String importCSR(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean importCertificate(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean importKeypair(String arg0, String arg1, String arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int loadKeypair(String arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Enumeration<String> loadLocalKeystore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeKeypair(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void resetLocalKeystore() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean saveKeypair(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean signCSR(String arg0, String arg1, String arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
