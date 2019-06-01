99c931145b7e526ae462f584eb0c21c34bff2d5a
mdragonborn <lena.markovic996@gmail.com>
2019-06-02 00:45:41 +0200

    Save keystore as attribute

--- src/implementation/MyCode.java
+++ src/implementation/MyCode.java
@@ -4,22 +4,47 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
 import java.security.cert.CertificateException;
+import java.security.spec.RSAKeyGenParameterSpec;
+import java.util.Date;
 import java.util.Enumeration;
 
+import javax.security.auth.x500.X500Principal;
+import javax.security.cert.X509Certificate;
+
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.jce.X509Principal;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+
 import code.GuiException;
+import gui.Constants;
 import x509.v3.CodeV3;
 
 public class MyCode extends CodeV3 {
 	
 	private final String keystore_file = "local_keystore.p12"; 
 	private final String keystore_pass = "qwe123";
+	
+	private KeyStore keyStore;
 
 	public MyCode(boolean[] algorithm_conf, boolean[] extensions_conf, boolean extensions_rules) throws GuiException {
 		super(algorithm_conf, extensions_conf, extensions_rules);
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		// TODO Auto-generated constructor stub
 	}
 
@@ -97,9 +122,8 @@ public class MyCode extends CodeV3 {
 
 	@Override
 	public Enumeration<String> loadLocalKeystore() {
-		KeyStore ks = null;
 		try {
-			ks = KeyStore.getInstance(KeyStore.getDefaultType());
+			keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
 		} catch (KeyStoreException e) {
 			e.printStackTrace();
 		}
@@ -108,9 +132,9 @@ public class MyCode extends CodeV3 {
 		
 		if(!file.exists()) {
 			try {
-				ks.load(null, this.keystore_pass.toCharArray());
+				keyStore.load(null, this.keystore_pass.toCharArray());
 				FileOutputStream os = new FileOutputStream(this.keystore_file);
-				ks.store(os, this.keystore_pass.toCharArray());
+				keyStore.store(os, this.keystore_pass.toCharArray());
 				os.close();
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -120,11 +144,11 @@ public class MyCode extends CodeV3 {
 	    FileInputStream fis = null;
 	    try {
 	        fis = new java.io.FileInputStream(this.keystore_file);
-	        ks.load(fis, this.keystore_pass.toCharArray());
+	        keyStore.load(fis, this.keystore_pass.toCharArray());
 	    }
 	    catch(java.io.EOFException e) {
 	    	try {
-				ks.load(null, this.keystore_pass.toCharArray());
+				keyStore.load(null, this.keystore_pass.toCharArray());
 			} catch (NoSuchAlgorithmException | CertificateException | IOException e1) {
 				e1.printStackTrace();
 			}
@@ -141,7 +165,7 @@ public class MyCode extends CodeV3 {
 	    }
 	    
 	    try {
-			return ks.aliases();
+			return keyStore.aliases();
 		} catch (KeyStoreException e) {
 			e.printStackTrace();
 		}
@@ -164,6 +188,7 @@ public class MyCode extends CodeV3 {
 			ks.load(null, this.keystore_pass.toCharArray());
 			FileOutputStream os = new FileOutputStream(this.keystore_file);
 			ks.store(os, this.keystore_pass.toCharArray());
+			keyStore = ks;
 			
 		} catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
 			e.printStackTrace();
